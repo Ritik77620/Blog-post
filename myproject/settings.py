0c2ea8fb95d7d4696e68cca38b4bb3bf82bb555e
Ritik77620 <177842599+Ritik77620@users.noreply.github.com>
2024-11-14 07:57:41 +0530

    Update settings.py
    BAsic dir

--- myproject/settings.py
+++ myproject/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMP_DIR = os.path.join(BASE_DIR,"templates")
 STATIC_DIR = os.path.join(BASE_DIR,"static")
 MEDIA_DIR = os.path.join(BASE_DIR,"media")
